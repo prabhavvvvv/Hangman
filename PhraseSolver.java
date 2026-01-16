d1d4ddc102e056231e44921288c34a75d51ed469
prabhaaaav <108563497+prabhavvvvv@users.noreply.github.com>
2026-01-16 11:36:58 -0800

    Update PhraseSolver.java

--- PhraseSolver.java
+++ PhraseSolver.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 public class PhraseSolver {
@@ -14,57 +15,50 @@ public class PhraseSolver {
     }
 
     public void play() {
-        Scanner input = new Scanner(System.in);
+        boolean solved = false;
         int currentPlayer = 1;
 
+        Scanner input = new Scanner(System.in);
+
+        boolean correct = true;
+
         while (!solved) {
-            board.setLetterValue();
+            System.out.println("Letter Guess Value:" + board.getCurrentLetterValue());
 
-            if (currentPlayer % 2 == 1) {
-                System.out.println("\n" + player1.getName() + " it is your turn to guess");
+            if (currentPlayer == 1) {
+                System.out.println("You have " + player1.getPoints() + " points.");
+                System.out.println(player1.getName() + " it's your turn.");
+
+                currentPlayer = 2;
             } else {
-                System.out.println("\n" + player2.getName() + " it is your turn to guess");
+                System.out.println(player2.getName() + " it's your turn.");
+                currentPlayer = 1;
             }
-
-            System.out.println("\nCurrent phrase: " + board.getSolvedPhrase());
-            System.out.println("Letter Value: " + board.getCurrentLetterValue());
-            System.out.println(player1.getName() + " points: " + player1.getPoints());
-            System.out.println(player2.getName() + " points: " + player2.getPoints());
-
-            String guess = "";
-            while (guess.length() != 1) {
-                System.out.print("Enter a letter: ");
-                guess = input.nextLine().trim();
-            }
-
-            boolean isCorrect = board.guessLetter(guess);
-
-            if (isCorrect) {
-                if (currentPlayer % 2 == 1) {
-                    player1.setPoints(player1.getPoints() + board.getCurrentLetterValue());
+            String playerGuess = input.nextLine();
+            if (board.guessLetter(playerGuess)) {
+                System.out.println("Correct!");
+                if(currentPlayer == 1) {
+                    player1.setPoints(board.getCurrentLetterValue() + player1.getPoints());
+                    currentPlayer = 1;
                 } else {
-                    player2.setPoints(player2.getPoints() + board.getCurrentLetterValue());
+                    player2.setPoints(board.getCurrentLetterValue() + player2.getPoints());
+                    currentPlayer = 2;
                 }
-                System.out.println("That is correct");
-            } else {
-                System.out.println("That is the wrong guess.");
-                currentPlayer++;
-            }
-
-            if (board.isSolved()) {
-                solved = true;
-                System.out.println("\nPhrase is solved!");
-                System.out.println(player1.getName() + " points: " + player1.getPoints());
-                System.out.println(player2.getName() + " points: " + player2.getPoints());
-
-                if (player1.getPoints() > player2.getPoints()) {
-                    System.out.println("Winner: " + player1.getName());
-                } else if (player2.getPoints() > player1.getPoints()) {
-                    System.out.println("Winner: " + player2.getName());
-                } else {
-                    System.out.println("It's a tie");
+                if (board.isSolved(playerGuess)) {
+                    solved = true;
+                    if (player1.getPoints() > player2.getPoints()) {
+                        System.out.println("Player 1 wins!");
+                    } else if (player1.getPoints() < player2.getPoints()) {
+                        System.out.println("Player 2 wins!");
+                    } else {
+                        System.out.println("It's a tie!");
+                    }
                 }
+                    
             }
+            System.out.println(board.getSolvedPhrase());
+
+            solved = true;
         }
     }
 }
